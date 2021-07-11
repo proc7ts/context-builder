b587e32cbdb210c1d0fa669005a7d631008076e9
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-07-11 19:05:02 +0700

    Add `CxGlobals`: Global values context (#11)
    * Add `CxGlobals`: Global values context
    
    * `@proc7ts/context-values` v7-dev.13

---
+++ src/entries/globals.entry.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from '@jest/globals';
+import { CxGlobals, CxReferenceError } from '@proc7ts/context-values';
+import { cxConstAsset } from '../assets';
+import { CxBuilder } from '../builder';
+
+describe('CxGlobals', () => {
+  it('has no default value', () => {
+
+    const cxBuilder = new CxBuilder(get => ({ get }));
+
+    expect(() => cxBuilder.get(CxGlobals)).toThrow(new CxReferenceError(CxGlobals));
+  });
+  it('can be provided', () => {
+
+    const cxBuilder = new CxBuilder(get => ({ get }));
+
+    cxBuilder.provide(cxConstAsset(CxGlobals, cxBuilder.context));
+
+    expect(cxBuilder.get(CxGlobals)).toBe(cxBuilder.context);
+  });
+});
